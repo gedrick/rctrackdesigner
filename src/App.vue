4762d145404401614173d670e0cf3fff26ee3375
Christopher Lee <christopher.lee@warnerbros.com>
2021-11-30 21:14:29 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="grid-container">
+    <div class="grid-row" v-for="(blockRow, rowIndex) in blocks" :key="rowIndex">
+      <GridItem
+        v-for="(blockItem, columnIndex) in blockRow"
+        :key="`${rowIndex}-${columnIndex}`"
+        :item="blockItem"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import GridItem from './components/GridItem.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    GridItem,
+  },
+  data() {
+    return {
+      width: 20,
+      height: 20,
+      blocks: [] as { type: string }[],
+      sampleRow: [
+        { type: 'default' },
+        { type: 'leftToRight' },
+        { type: 'leftToRight' },
+        { type: 'leftToUp' },
+      ],
+    };
+  },
+  mounted() {
+    // Set up default blocks
+    this.blocks = [...this.sampleRow, ...this.sampleRow, ...this.sampleRow, ...this.sampleRow];
   },
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +47,18 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.grid-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+.grid-row {
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
 }
 </style>
---
+++ src/components/GridItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid-item" :class="item.type">{{ item.type }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GridItem',
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {};
+      },
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.grid-item {
+  background-color: grey;
+  width: 100px;
+  height: 100px;
+}
+</style>
